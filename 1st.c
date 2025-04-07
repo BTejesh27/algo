bacf8573dd349bd10a9be759bdb1e52e5f77be23
BTejesh27 <bvst27@gmail.com>
2025-04-07 09:23:56 +0530

    aaa

--- 1st.c
+++ 1st.c
@@ -1,47 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#define POLYNOMIAL 0xD8
+#define WIDTH 8
+#define TOPBIT (1 << (WIDTH - 1))
 
-void characterStuffing(const char* input, char* output) {
-    const char FLAG = 'F';
-    const char ESC = 'E';
-    int j = 0;
-    output[j++] = FLAG;
-    for (int i = 0; input[i] != '\0'; i++) {
-        if (input[i] == FLAG || input[i] == ESC) {
-            output[j++] = ESC;
-        }
-        output[j++] = input[i];
-    }
-    output[j++] = FLAG;
-    output[j] = '\0';
-}
-
-void bitStuffing(const char* input, char* output) {
-    int count = 0, j = 0;
-    for (int i = 0; input[i] != '\0'; i++) {
-        output[j++] = input[i];
-        if (input[i] == '1') {
-            count++;
-            if (count == 5) {
-                output[j++] = '0';
-                count = 0;
+unsigned char crc8(unsigned char *data, size_t length) {
+    unsigned char remainder = 0;
+    for (size_t byte = 0; byte < length; ++byte) {
+        remainder ^= data[byte];
+        for (unsigned char bit = 8; bit > 0; --bit) {
+            if (remainder & TOPBIT) {
+                remainder = (remainder << 1) ^ POLYNOMIAL;
+            } else {
+                remainder = (remainder << 1);
             }
-        } else {
-            count = 0;
         }
     }
-    output[j] = '\0';
+    return remainder;
 }
 
 int main() {
-    char input[100], stuffed[200];
-    printf("Enter data for Character Stuffing: ");
-    scanf("%s", input);
-    characterStuffing(input, stuffed);
-    printf("Character Stuffed Output: %s\n", stuffed);
-    printf("\nEnter binary data for Bit Stuffing: ");
-    scanf("%s", input);
-    bitStuffing(input, stuffed);
-    printf("Bit Stuffed Output: %s\n", stuffed);
+    unsigned char message[] = "Hello, CRC!";
+    size_t messageLength = strlen((char *)message);
+    unsigned char crc = crc8(message, messageLength);
+    printf("Message: %s\n", message);
+    printf("CRC: 0x%02X\n", crc);
     return 0;
 }
