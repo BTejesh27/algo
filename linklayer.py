81a8b735cebc9dae0e5b5c3adfd61f4b7b5b70e4
BTejesh27 <145187809+BTejesh27@users.noreply.github.com>
2025-04-23 21:24:06 +0530

    Update linklayer.py

--- linklayer.py
+++ linklayer.py
@@ -7,15 +7,6 @@ def char_stuff(d):
         s += c
     return s + f
 
-def char_unstuff(d):
-    f, e = 'F', 'E'
-    i, s = 1, ''
-    while i < len(d) - 1:
-        if d[i] == e:
-            i += 1
-        s += d[i]
-        i += 1
-    return s
 
 def bit_stuff(d):
     s = ''
@@ -31,20 +22,6 @@ def bit_stuff(d):
             c = 0
     return s
 
-def bit_unstuff(d):
-    s = ''
-    c, i = 0, 0
-    while i < len(d):
-        s += d[i]
-        if d[i] == '1':
-            c += 1
-            if c == 5:
-                i += 1
-                c = 0
-        else:
-            c = 0
-        i += 1
-    return s
 
 mode = input("Enter mode (char/bit): ")
 data = input("Enter data: ")
@@ -52,12 +29,10 @@ data = input("Enter data: ")
 if mode == 'char':
     stf = char_stuff(data)
     print("Stuffed:", stf)
-    print("Unstuffed:", char_unstuff(stf))
 
 elif mode == 'bit':
     stf = bit_stuff(data)
     print("Bit Stuffed:", stf)
-    print("Bit Unstuffed:", bit_unstuff(stf))
 
 else:
     print("Invalid mode")
