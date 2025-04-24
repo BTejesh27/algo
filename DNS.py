45a22e1ef6b27b46532add804c80cb33d71ebd54
BTejesh27 <145187809+BTejesh27@users.noreply.github.com>
2025-04-24 19:03:34 +0530

    Delete DNS.py

--- DNS.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-host = input("Enter hostname: ")
-
-try:
-    ip = socket.gethostbyname(host)
-    print("IP address of", host, "is", ip)
-except socket.gaierror:
-    print("Hostname not found!")
